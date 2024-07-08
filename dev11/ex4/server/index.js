3fd33b4d7cad3a82375d2e2151e836b304906292
AdarAfek <adar257@gmail.com>
2024-07-08 15:03:19 +0300

    Project Init

---
+++ dev11/ex4/server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const Vactions = require('./data/books.json');
+app.use((req, res, next) => {
+ res.set({
+ 'Access-Control-Allow-Origin': '*',
+ 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+ 'Access-Control-Allow-Methods': "GET, POST, PUT, DELETE",
+ 'Content-Type': 'application/json'
+ });
+ next();
+});
+app.listen(port);
+console.log(`listening on port ${port}`);
---
+++ dev11/ex4/server/routers/vactionsRouter.js
@@ -0,0 +1,11 @@
+
+const { Router } = require("express");
+const { vactionController } = require("../controllers/VactionsController");
+const userRouter = new Router();
+
+userRouter.get("/", );
+userRouter.post("", ); 
+userRouter.put("/", ); 
+userRouter.delete("/",); 
+
+module.exports = { vactionRouter };
