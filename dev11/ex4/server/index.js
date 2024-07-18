0c6a7f36c0a5246bf4ddaceaf9650876033f4b94
AdarAfek <adar257@gmail.com>
2024-07-18 18:52:06 +0300

    add user router to the index

--- dev11/ex4/server/index.js
+++ dev11/ex4/server/index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 8080;
-const db = require("../server/db");
+const db = require("./db");
 const dbconnect = db.createConnection;
-
+app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.set({
     "Access-Control-Allow-Origin": "*",
@@ -15,6 +16,20 @@ app.use((req, res, next) => {
   });
   next();
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//User routes
+const { userRouter } = require("./routers/usersRouter");
+app.use("/api/user",userRouter);
+
+
+
+
+
+
+
+
+
 
 app.listen(port);
 console.log(`listening on port ${port}`);
--- dev11/ex4/server/routers/usersRouter.js
+++ dev11/ex4/server/routers/usersRouter.js
@@ -1,11 +1,1 @@
 
-const { Router } = require("express");
-const { vactionController } = require("../controllers/VactionsController");
-const userRouter = new Router();
-
-userRouter.get("/", );
-userRouter.post("", ); 
-userRouter.put("/", ); 
-userRouter.delete("/",); 
-
-module.exports = { vactionRouter };
